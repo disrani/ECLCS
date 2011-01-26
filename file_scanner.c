a7948227ee0116b97ecd8ef97865f5ed8b40da3d
Dinesh Israni <ddi@andrew.cmu.edu>
2011-01-26 01:36:26 -0500

    Reads 10 bytes after every increment of chunk size. This ensures that every chunk is accessed only once. Bad performance with LRU

---
+++ file_scanner.c
@@ -0,0 +1,63 @@
+#define _LARGEFILE64_SOURCE
+#define _FILE_OFFSET_BITS 64
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUF_SIZE 100
+#define CHUNK_SIZE  128 * 1024	// 128 KB
+
+int
+main(int argc, char **argv)
+{
+	char *fname;
+	int fd;
+	char buf[BUF_SIZE];
+	off64_t ret, fsize;
+	struct stat st;
+
+	if (argc != 2) {
+		printf("Usage: %s <file_name>\n", argv[0]);
+		exit(0);
+	}
+
+
+	fname = argv[1];
+	fd = open(fname, O_RDONLY);
+	if (fd < 0) {
+		perror("Error opening file\n");
+		exit(0);
+	}
+
+	stat(fname, &st);
+	fsize = st.st_size;
+	
+	while(1) {
+		read(fd, buf, BUF_SIZE);
+		ret = lseek64(fd, CHUNK_SIZE, SEEK_CUR);
+		printf("%lld\n", ret);
+		if (ret > fsize) {
+			lseek64(fd, 0, SEEK_SET);
+			exit(0);
+		}
+		/*
+		if (ret < 0) {
+			if (errno == EINVAL) {
+				printf("Reached end of file\n");
+				lseek64(fd, 0, SEEK_SET);
+			} else {
+				perror("Error seeking\n");
+				lseek64(fd, 0, SEEK_SET);
+			}
+		}
+		*/
+	}
+
+	close(fd);
+	return 0;
+}
